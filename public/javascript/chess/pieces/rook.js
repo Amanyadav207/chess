7a7417e7a81da8605e732dc9ebb531dbe486b95d
Devi Charan <145871325+cherry-1729-9090@users.noreply.github.com>
2024-09-26 20:11:20 +0530

    Done with rook code

--- public/javascript/chess/pieces/rook.js
+++ public/javascript/chess/pieces/rook.js
@@ -1,11 +1,29 @@
-var Rook = function(config){
-    this.type = 'rook';
-    this.constructor(config);
+var Piece = function(config) {
+    this.position = config.position || { x: 0, y: 0 };
+    this.color = config.color || 'white';
 };
 
+Piece.prototype.move = function(newPosition) {
+    this.position = newPosition;
+};
 
+var Rook = function(config) {
+    this.type = 'rook';
+    Piece.call(this, config);
+};
 
-Rook.prototype = new Piece({});
-Rook.prototype.move = function(newPosition){
+Rook.prototype = Object.create(Piece.prototype);
 
-}
+Rook.prototype.move = function(newPosition) {
+    const currentX = this.position.x;
+    const currentY = this.position.y;
+    const newX = newPosition.x;
+    const newY = newPosition.y;
+
+    if (newX === currentX || newY === currentY) {
+        this.position = newPosition;
+        console.log(`Rook moved to (${newX}, ${newY})`);
+    } else {
+        console.log("Invalid move for a rook! It can only move in straight lines.");
+    }
+};
